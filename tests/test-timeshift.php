04c65c01bea24ca4b9032446a7356fdd5454ae04
Claudio Frasca <c.frasca@krone.at>
2018-04-11 17:14:30 +0200

    unit tests

--- tests/test-timeshift.php
+++ tests/test-timeshift.php
@@ -29,10 +29,89 @@ class TestTimeshift extends \WP_UnitTestCase
         $this->core = new Core('i18n');
     }
 
-    public function testA() {
-      $this->assertEquals(1,1);
+    /**
+    * @test
+    */
+    public function hasTimeshiftsTrue() {
+        $post_id = $this->factory->post->create(['post_type' => "article"]);
+        $post = get_post($post_id);
+        $post_type = get_post_type($post_id);
+        $table_name = 'wptesttimeshift_article';
+        $objArr = array('amount' => '1');
+        $objArr = (object) $objArr;
+        $arr = array($objArr);
+
+        //Mock the DB
+        $mock = $this->getMockBuilder('KMM\\Timeshift\\KMM\\TimeshiftTestDB')
+            ->setMethods(array( 'get_charset_collate', 'get_results' ))
+            ->getMock();
+
+        $mock->prefix = "wptest";
+
+        //Expect query sent
+        $mock->expects($this->once())
+            ->method('get_results')
+            ->with('select count(1) as amount from ' . $table_name . ' where post_id=' . $post_id)
+            ->willReturn($arr);
+
+        $this->core->wpdb = $mock;
+
+        $r = $this->core->hasTimeshifts($post_id);
+
+        $this->assertTrue($r);
     }
 
+    /**
+    * @test
+    */
+    public function hasTimeshiftsFalse() {
+        $post_id = $this->factory->post->create(['post_type' => "article"]);
+        $post_type = get_post_type($post_id);
+        $table_name = 'wptesttimeshift_article';
+
+        //Mock the DB
+        $mock = $this->getMockBuilder('KMM\\Timeshift\\KMM\\TimeshiftTestDB')
+            ->setMethods(array( 'get_charset_collate', 'get_results' ))
+            ->getMock();
+
+        $mock->prefix = "wptest";
+
+        //Expect query sent
+        $mock->expects($this->once())
+            ->method('get_results')
+            ->with('select count(1) as amount from ' . $table_name . ' where post_id=' . $post_id);
+
+        $this->core->wpdb = $mock;
+
+        $r = $this->core->hasTimeshifts($post_id);
+
+        $this->assertFalse($r);
+    }
+
+    /**
+    * @test
+    */
+    public function checkTable() {
+        $post_id = $this->factory->post->create(['post_type' => "article"]);
+        $post_type = get_post_type($post_id);
+
+        //Mock the DB
+        $mock = $this->getMockBuilder('KMM\\Timeshift\\KMM\\TimeshiftTestDB')
+            ->setMethods(array( 'get_charset_collate' ))
+            ->getMock();
+
+        $mock->prefix = "wptest";
+
+        //Expect query sent
+        $mock->expects($this->once())
+            ->method('get_charset_collate');
+
+        $this->core->wpdb = $mock;
+
+        $r = $this->core->checkTable($post_type);
+
+        $this->assertTrue($r);
+    }
 
     public function tearDown()
     {
